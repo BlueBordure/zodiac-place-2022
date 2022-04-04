9eb684ad43df3e712f057cee20eabcf457930fd9
BlueBordure <102932393+BlueBordure@users.noreply.github.com>
2022-04-04 09:06:35 +0800

    Add script

---
+++ userscript.user.js
@@ -0,0 +1,32 @@
+// ==UserScript==
+// @name         Zodiac Place template
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the canvas!
+// @author       oralekin, blue_bordure
+// @match        https://hot-potato.reddit.com/embed*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=reddit.com
+// @grant        none
+// ==/UserScript==
+if (window.top !== window.self) {
+  window.addEventListener(
+    "load",
+    () => {
+      document
+        .getElementsByTagName("mona-lisa-embed")[0]
+        .shadowRoot.children[0].getElementsByTagName("mona-lisa-canvas")[0]
+        .shadowRoot.children[0].appendChild(
+          (function () {
+            const i = document.createElement("img");
+            i.src =
+              "https://github.com/BlueBordure/zodiac-place-2022/raw/main/zodiac_place_template.png";
+            i.style =
+              "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 2000px;height: 2000px;";
+            console.log(i);
+            return i;
+          })()
+        );
+    },
+    false
+  );
+}
